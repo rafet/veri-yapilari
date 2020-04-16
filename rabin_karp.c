e4f360be96dbaf1ba3cf6dac61718bc2facf2d5a
Rafet Topçu <rafet1998@gmail.com>
2020-04-16 21:47:47 +0300

    removed unnecessary things and optimized

--- rabin_karp.c
+++ rabin_karp.c
@@ -16,46 +16,34 @@ int calcSingleHash(string txt)
 
     return hash;
 }
-int *calculateHashes(string text, string pattern)
+int find(string text, string pattern)
 {
-    static int hashes[100];
     int n = strlen(text),
         lastHash = 0,
         patternLength = strlen(pattern),
+        patternHash = calcSingleHash(pattern),
         i, k = 0;
 
     for (i = 0; i < patternLength; i++)
         lastHash += val(text[i]) * pow(prime, i);
+    if (lastHash == patternHash)
+        return 0;
 
-    hashes[k++] = lastHash;
     for (i = 1; i < n - patternLength + 1; i++)
     {
         lastHash = (lastHash - val(text[i - 1])) / prime;
         lastHash += val(text[i + patternLength - 1]) * pow(prime, patternLength - 1);
-        hashes[k++] = lastHash;
-    }
-    return hashes;
-}
-
-int find(string text, string pattern)
-{
-    int *hashes = calculateHashes(text, pattern);
-    int hashesLength = strlen(text) - strlen(pattern) + 1;
-    int patternHash = calcSingleHash(pattern), i;
-
-    for (i = 0; i < hashesLength; i++)
-        if (patternHash == hashes[i])
+        if (lastHash == patternHash)
             return i;
-
+    }
     return -1;
 }
-
 int main()
 {
     string text = "rafettopcu";
     string pattern = "etto";
-
     int index = find(text, pattern);
+
     if (index > -1)
         printf("Found in index %d", index);
     else
